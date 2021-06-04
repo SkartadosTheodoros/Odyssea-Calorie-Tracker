0d68253c13fad839fdeddaf2596aae4311aa6d4b
Thodoris Skartados <skartadostheodoros@gmail.com>
2021-06-05 02:50:01 +0300

    Add Modal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+import "./Modal.css"
+
+const Modal = (props)=>{
+    return(
+        <Fragment>
+            <div className="backdrop" />
+            <div className="modal"> hello</div>
+        </Fragment>
+
+    )
+}
+export default Modal

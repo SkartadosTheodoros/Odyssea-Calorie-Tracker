96472b3a1976e57db8a411727344807cc9bbe00c
Thodoris Skartados <skartadostheodoros@gmail.com>
2021-06-09 16:30:19 +0300

    Add date and meal type css style

--- src/components/CaloriesFilter/CaloriesFilter.js
+++ src/components/CaloriesFilter/CaloriesFilter.js
@@ -14,22 +14,24 @@ const CaloriesFilter = (props) => {
     }
 
     return (
-        <div className="filter-entry">
-            <div className="filter-entry-meal">
-                <label htmlFor="meal">Meal Type:</label>
-                <select name="meals" id="meal" onChange={startMealChangeHandler}>
-                    <option value="all">all</option>
-                    {
-                        props.mealType.length > 0 && props.mealType[0] != null
-                            ? props.mealType.map(type => { return (<option value={type}>{type}</option>) })
-                            : null
-                    }
-                </select>
-            </div>
+        <div className="filter">
+            <div className="filter-entry">
+                <div className="filter-entry-meal">
+                    <label htmlFor="meal">Meal Type : </label>
+                    <select name="meals" id="meal" onChange={startMealChangeHandler}>
+                        <option value="all">all</option>
+                        {
+                            props.mealType.length > 0 && props.mealType[0] != null
+                                ? props.mealType.map(type => { return (<option value={type}>{type}</option>) })
+                                : null
+                        }
+                    </select>
+                </div>
 
-            <div className="filter-entry-date">
-                <label>Start on</label>
-                <input type="date" value={props.startDate} onChange={startDateChangeHandler} />
+                <div className="filter-entry-date">
+                    <label>Start on : </label>
+                    <input type="date" value={props.startDate} onChange={startDateChangeHandler} />
+                </div>
             </div>
         </div>
     )

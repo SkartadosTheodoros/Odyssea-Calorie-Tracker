3e4aa4ae0f3cefff923275fcea33ea8ec03f679e
Theodoros Skartados <skartadostheodoros@gmail.com>
2021-06-17 07:37:51 +0300

    Update Error modal functions and style

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -9,13 +9,18 @@ const ErrorModal = (props) => {
                     <h2>{props.title}</h2>
                 </div>
                 <div className="error-content">
-                    <p>Input must have one of the following forms:</p>
-                    <ul>
-                        <li>200g green salad</li>
-                        <li>100g burger for meal</li>
-                        <li>100g burger and 200g green salad</li>
-                        <li>100g burger and 200g green salad for dinner</li>
-                    </ul>
+                    {(props.message === "Meal Wrong Input Format")
+                        ?
+                        <div>
+                            <p>Input must have one of the following forms:</p>
+                            <ul>
+                                <li>200<b>g</b> green salad</li>
+                                <li>10<b>lb</b> beef brisket <b>for</b> meal</li>
+                                <li>14<b>oz</b> prime rib <b>and</b> mashed potatoes <b>for</b> dinner</li>
+                            </ul>
+                        </div>
+                        :
+                        <p>{props.message}</p>}
                 </div>
                 <footer className="error-button">
                     <button type="button" onClick={props.onDismiss}>OK</button>
